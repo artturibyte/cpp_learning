e53017c8b9f225250389096d8df79ceb884f5c9a
Artturi Mard <artturimard@gmail.com>
2024-09-14 14:32:59 +0300

    misc: cpp with C

---
+++ miscellanious/cpp_with_c.cpp
@@ -0,0 +1,4 @@
+/*
+Almost all C programs are compatible with c++. Some incompatible features with C99.
+- Easiest way is to recompile program with C++ compiler, if possible.
+*/
---
+++ miscellanious/interfacing_with_c/assignment/print.h
@@ -0,0 +1,6 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+int print_array(int arr[], int n);
+
+#endif //ARRAY_H
